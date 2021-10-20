273c4397658056d48acdf7ccf90ebda7a8aae26e
chris <chris@bumblehead.com>
2021-10-20 00:56:56 -0700

    small changes to readme

--- pkce-pair.mjs
+++ pkce-pair.mjs
@@ -12,61 +12,51 @@ const cryptoResolve = crypto => {
   return cryptoResolved;
 };
 
-// https://tools.ietf.org/html/rfc4648#section-5
-const b64Uri = str => str
-  .replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-
-const b64UriFromBinary = str => b64Uri(btoa(str));
-
 const randomIndicesCreate = (crypto, size) => {
   crypto = cryptoResolve(crypto);
 
   return crypto.getRandomValues
-    ? crypto.getRandomValues(new Uint8Array(size)) // browser crypto
-    : crypto.randomBytes(size); // node crypto
+    ? crypto.getRandomValues(new Uint8Array(size))
+    : crypto.randomBytes(size);
 };
 
-const random = (crypto, size, mask, result = '') => {
-  const randomIndices = randomIndicesCreate(crypto, size);
+const mask =
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~';
+
+const createVerifier = (crypto, len, result = '') => {
+  const randomIndices = randomIndicesCreate(crypto, len);
   const byteLength = Math.pow(2, 8); // 256
   const maskLength = Math.min(mask.length, byteLength);
   // the scaling factor breaks down the possible values of bytes (0x00-0xFF)
   // into the range of mask indices
   const scalingFactor = byteLength / maskLength;
-  while (size--) {
-    result += mask[Math.floor(randomIndices[size] / scalingFactor)];
-  }
+  while (len--)
+    result += mask[Math.floor(randomIndices[len] / scalingFactor)];
 
   return result;
 };
 
-// eslint-disable-next-line max-len
-const charMask = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~';
+const createSHA256Digest = (crypto, str) => new Uint8Array(crypto.subtle.digest(
+  'SHA-256', new Uint8Array(str.length).map((_, i) => str.charCodeAt(i))));
 
-const createVerifier = (crypto, len) => random(crypto, len, charMask);
+const createAStrFromDigest = dig => btoa(String.fromCharCode.apply(0, dig));
 
 const createChallenge = async (crypto, verifier) => {
   crypto = cryptoResolve(crypto);
 
-  if (crypto.createHash)
-    return b64Uri(
-      crypto.createHash('sha256').update(verifier).digest('base64'));
-
-  const digest = await crypto.subtle.digest(
-    'SHA-256', new Uint8Array(verifier.length)
-      .map((num, i) => verifier.charCodeAt(i)));
-
-  return b64UriFromBinary(
-    String.fromCharCode.apply(null, (new Uint8Array(digest)))); 
+  return ( // https://tools.ietf.org/html/rfc4648#section-5
+    crypto.createHash
+      ? crypto.createHash('sha256').update(verifier).digest('base64')
+      : createAStrFromDigest(createSHA256Digest(crypto, verifier))
+  ).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 };
 
 const isVerified = async (crypto, verifier, challenge) => (
   await createChallenge(crypto, verifier, crypto)) === challenge;
 
 const create = async (crypto, len = 43) => {
-  if (len < 43 || len > 128) {
+  if (len < 43 || len > 128)
     throw new Error(`Expected a length between 43 and 128. Received ${len}.`);
-  }
 
   const verifier = createVerifier(crypto, len);
 
